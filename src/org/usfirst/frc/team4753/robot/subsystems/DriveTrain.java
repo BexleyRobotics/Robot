19038fef6a2f78b41bfcffd75d3a49e1d238ef0f
nana8901 <nana8901@gmail.com>
2016-02-22 16:30:08 -0500

    drivetrain stuff

--- src/org/usfirst/frc/team4753/robot/subsystems/DriveTrain.java
+++ src/org/usfirst/frc/team4753/robot/subsystems/DriveTrain.java
@@ -7,12 +7,14 @@ import edu.wpi.first.wpilibj.command.Subsystem;
  */
 public class DriveTrain extends Subsystem {
     
-    // Put methods for controlling this subsystem
-    // here. Call these from Commands.
 
     public void initDefaultCommand() {
         // Set the default command for a subsystem here.
         //setDefaultCommand(new MySpecialCommand());
+    }
+    public void joystickInput(Joystick)
+    {
+    	robot.tankdrive(left, right);
     }
 }
 
